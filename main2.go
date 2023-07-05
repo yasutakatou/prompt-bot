40fad5c096ba897d9a6f48c2e3d00e602fb7a8cb
yasutakatou <fzk01325@nifty.com>
2023-07-05 23:35:48 +0900

    Update main2.go

--- main2.go
+++ main2.go
@@ -11,43 +11,47 @@ var (
 )
 
 func main() {
-	all := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+	all = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
 	rand.Seed(time.Now().Unix())
 	alls := len(all)
-	fmt.Println(alls)
+	//fmt.Println(alls)
 	rnd := rand.Intn(len(all))
-	fmt.Println(rnd)
+	//fmt.Println(rnd)
 	if rnd >= (alls / 2) {
+		fmt.Println("uploop")
 		uploop(rnd, alls)
 	} else {
+		fmt.Println("downloop")
 		downloop(rnd, alls)
 	}
 }
 
 func uploop(rnd, alls int) {
 	cnt := alls
-	for i := rnd; i <= len(all); i++ {
+	for i := rnd; i < len(all); i++ {
 		fmt.Println(all[i])
 		cnt = cnt - 1
 	}
 
-	for i := 0; i <= cnt; i++ {
+	for i := 0; i < cnt; i++ {
 		fmt.Println(all[i])
 	}
 }
 
 func downloop(rnd, alls int) {
 	i := rnd
-	for {
-		fmt.Println(all[i])
-		i = i - 1
-		if i == 0 {
-			break
+	if rnd > 0 {
+		for {
+			fmt.Println(i)
+			i = i - 1
+			if i == 0 {
+				break
+			}
 		}
 	}
 
-	count := alls - rnd
+	count := alls - rnd - 1
 	for i := 0; i <= count; i++ {
-		fmt.Println(all[alls-i])
+		fmt.Println(all[alls-i-1])
 	}
 }
